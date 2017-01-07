e50f5d11a10946e2901e1d02eacba6acf076a372
Mao Wtm <micromaomao@gmail.com>
2017-01-07 21:32:03 +0800

    Basic interface for noscript

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const os = require('os')
 const PaperUtils = require('./view/paperutils')
+const CIESubjects = require('./view/CIESubjects')
 const sspdf = require('./lib/sspdf')
 const fs = require('fs')
 const SVGO = require('svgo')
+const pug = require('pug')
+
 const svgo = new SVGO()
 
 module.exports = (db, mongoose) => {
@@ -28,81 +31,100 @@ module.exports = (db, mongoose) => {
       }, err => next(err))
   })
 
+  function doSearch (query) {
+    return new Promise((resolve, reject) => {
+      let match
+      if (query.match(/^\d{4}$/)) {
+        fetchPP(query, null, null, null, null)
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})$/))) {
+        fetchPP(match[1], match[2])
+      } else if ((match = query.match(/^(\d{4})[_ ](\d)$/))) {
+        fetchPP(match[1], null, match[2], null, null)
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]+)$/))) {
+        fetchPP(match[1], null, null, null, match[2])
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]([a-z]+)$/))) {
+        fetchPP(match[1], match[2], null, null, match[3])
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]*(paper[_ ]*)?(\d)$/))) {
+        fetchPP(match[1], match[2], match[4], null, null)
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]*(paper[_ ]*)?(\d)(\d)$/))) {
+        fetchPP(match[1], match[2], match[4], match[5], null)
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]([a-z]+)[_ ](\d)(\d)$/))) {
+        fetchPP(match[1], match[2], match[4], match[5], match[3])
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ](\d)(\d)[_ ]([a-z]+)$/))) {
+        fetchPP(match[1], match[2], match[3], match[4], match[5])
+      } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]([a-z]+)[_ ](\d)$/))) {
+        fetchPP(match[1], match[2], match[4], null, match[3])
+      } else if ((match = query.toUpperCase().match(/^(\d{4})\/(\d{2})\/([A-Z]\/[A-Z]|SP)\/(\d{2})$/))) {
+        let month = PaperUtils.odashMonthToMyMonth(match[3])
+        let time = month + match[4]
+        let [paper, variant] = match[2].split('')
+        fetchPP(match[1], time, paper, variant, null)
+      } else {
+        PastPaperIndex.search(query).then(results => {
+          Promise.all(results.map(rst => new Promise((resolve, reject) => {
+            PastPaperDoc.find({subject: rst.doc.subject, time: rst.doc.time, paper: rst.doc.paper, variant: rst.doc.variant}, {_id: true, type: true, fileType: true, numPages: true}, (err, res) => {
+              if (err) {
+                resolve({doc: rst.doc, index: rst.index, related: []})
+              } else {
+                resolve({doc: rst.doc, index: rst.index, related: res.filter(x => x.type !== rst.doc.type)})
+              }
+            })
+          }))).then(rst => resolve({
+            response: 'text',
+            list: rst
+          }), err => reject({response: 'error', err: err.toString()}))
+        }).catch(err => {
+          reject({response: 'error', err: err.toString()})
+        })
+      }
+
+      function fetchPP (subject, time, paper, variant, type) {
+        let finder = {}
+        subject && (finder.subject = subject)
+        time && (finder.time = time.toLowerCase())
+        if (parseInt(paper) === 0 && Number.isSafeInteger(parseInt(variant)) && parseInt(variant) !== 0) {
+          paper = variant
+          variant = '0'
+        }
+        paper && (finder.paper = parseInt(paper))
+        variant && (finder.variant = parseInt(variant))
+        type && (finder.type = type.toLowerCase())
+        PastPaperDoc.find(finder, {__v: false, doc: false}).limit(51).then(rst => {
+          if (rst.length >= 50) {
+            resolve({
+              response: 'overflow'
+            })
+            return
+          }
+          resolve({
+            response: 'pp',
+            list: rst
+          })
+        }, err => {
+          reject({response: 'error', err: err.toString()})
+        })
+      }
+    })
+  }
+
   rMain.get('/search/:query', function (req, res, next) {
     let query = req.params.query.toString().trim()
-    let match
-    if (query.match(/^\d{4}$/)) {
-      fetchPP(query, null, null, null, null)
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})$/))) {
-      fetchPP(match[1], match[2])
-    } else if ((match = query.match(/^(\d{4})[_ ](\d)$/))) {
-      fetchPP(match[1], null, match[2], null, null)
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]+)$/))) {
-      fetchPP(match[1], null, null, null, match[2])
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]([a-z]+)$/))) {
-      fetchPP(match[1], match[2], null, null, match[3])
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]*(paper[_ ]*)?(\d)$/))) {
-      fetchPP(match[1], match[2], match[4], null, null)
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]*(paper[_ ]*)?(\d)(\d)$/))) {
-      fetchPP(match[1], match[2], match[4], match[5], null)
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]([a-z]+)[_ ](\d)(\d)$/))) {
-      fetchPP(match[1], match[2], match[4], match[5], match[3])
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ](\d)(\d)[_ ]([a-z]+)$/))) {
-      fetchPP(match[1], match[2], match[3], match[4], match[5])
-    } else if ((match = query.match(/^(\d{4})[_ ]([a-z]\d{2})[_ ]([a-z]+)[_ ](\d)$/))) {
-      fetchPP(match[1], match[2], match[4], null, match[3])
-    } else if ((match = query.toUpperCase().match(/^(\d{4})\/(\d{2})\/([A-Z]\/[A-Z]|SP)\/(\d{2})$/))) {
-      let month = PaperUtils.odashMonthToMyMonth(match[3])
-      let time = month + match[4]
-      let [paper, variant] = match[2].split('')
-      fetchPP(match[1], time, paper, variant, null)
-    } else {
-      PastPaperIndex.search(query).then(results => {
-        Promise.all(results.map(rst => new Promise((resolve, reject) => {
-          PastPaperDoc.find({subject: rst.doc.subject, time: rst.doc.time, paper: rst.doc.paper, variant: rst.doc.variant}, {_id: true, type: true, fileType: true, numPages: true}, (err, res) => {
-            if (err) {
-              resolve({doc: rst.doc, index: rst.index, related: []})
-            } else {
-              resolve({doc: rst.doc, index: rst.index, related: res.filter(x => x.type !== rst.doc.type)})
-            }
-          })
-        }))).then(rst => res.send({
-          response: 'text',
-          list: rst
-        })).catch(e => next(e))
-      }).catch(err => {
-        next(err)
-      })
+    doSearch(query).then(rst => res.send(rst), err => {
+      res.status(500)
+      res.send(err)
+    })
+  })
+  let _noscriptResultPage = pug.compileFile(path.join(__dirname, 'view/noscriptsearch.pug'))
+  let noscriptResultPage = obj => _noscriptResultPage(Object.assign({}, {PaperUtils, CIESubjects}, obj))
+  rMain.get('/noscript-search', function (req, res, next) {
+    let query = req.query.query.toString().trim()
+    if (query.length === 0) {
+      res.redirect('/')
+      return
     }
-
-    function fetchPP (subject, time, paper, variant, type) {
-      let finder = {}
-      subject && (finder.subject = subject)
-      time && (finder.time = time.toLowerCase())
-      if (parseInt(paper) === 0 && Number.isSafeInteger(parseInt(variant)) && parseInt(variant) !== 0) {
-        paper = variant
-        variant = '0'
-      }
-      paper && (finder.paper = parseInt(paper))
-      variant && (finder.variant = parseInt(variant))
-      type && (finder.type = type.toLowerCase())
-      console.log(finder)
-      PastPaperDoc.find(finder, {__v: false, doc: false}).limit(51).then(rst => {
-        if (rst.length >= 50) {
-          res.send({
-            response: 'overflow'
-          })
-          return
-        }
-        res.send({
-          response: 'pp',
-          list: rst
-        })
-      }, err => {
-        res.status(500)
-        res.send({response: 'error', err: err})
-      })
-    }
+    doSearch(query).then(rst => res.send(noscriptResultPage({rst, query})), err => {
+      next(err.err)
+    })
   })
 
   rMain.get('/fetchDoc/:id', function (req, res, next) {
