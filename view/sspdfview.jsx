c826eac41dd8bdec2237382f4dde447677fcb9c0
Mao Wtm <micromaomao@gmail.com>
2017-01-31 18:21:47 +0800

    Fix typo

--- view/sspdfview.jsx
+++ view/sspdfview.jsx
@@ -286,7 +286,7 @@ class SsPdfView extends React.Component {
       et.addEventListener('mouseup', this.handleUp, noPassiveEventsArgument)
       et.addEventListener('touchend', this.handleUp, noPassiveEventsArgument)
       et.addEventListener('touchcancel', this.handleUp, noPassiveEventsArgument)
-      et.addEventListener('mousewheel', this.handleUp, noPassiveEventsArgument)
+      et.addEventListener('mousewheel', this.handleScroll, noPassiveEventsArgument)
     }
   }
   componentWillReceiveProps (nextProps) {
