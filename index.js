d9d7be578de15f6264a183442704299bca834324
Mao Wtm <micromaomao@gmail.com>
2017-01-31 20:06:01 +0800

    More detailed logging

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ module.exports = (db, mongoose) => {
   }
 
   rMain.use(function (req, res, next) {
-    console.log(`Serving ${req.method.toUpperCase()} ${req.path} for ${req.ip}...`)
+    console.log(`Serving ${req.method.toUpperCase()} ${req.path} on ${req.hostname} for ${req.ip}...`)
     if (req.hostname.match(/^www\./)) {
       res.redirect('https://schsrch.xyz' + req.path)
     } else {
