82c160ee5dfeffa116ce812bba81ee3b10a6e42a
Mao Wtm <micromaomao@gmail.com>
2017-04-08 12:50:19 +0800

    Add test for doc with and without ?as=blob

--- test/getdoc.js
+++ test/getdoc.js
@@ -5,47 +5,51 @@ const crypto = require('crypto')
 module.exports = (schsrch, dbModel) =>
   describe('Getting the document', function () {
     const {PastPaperDoc, PastPaperIndex} = dbModel
-    it('/doc/?as=blob', function (done) {
-      PastPaperDoc.find({subject: '0610', time: 's17', paper: 1, variant: 1, type: 'qp'}).then(docs => {
-        if (!docs || docs.length !== 1) {
-          done(new Error(`There should be one and only one 0610_s17_1_1_qp in the testing database (there are currently ${docs.length}).`))
-          return
-        }
-        let tDoc = docs[0]
-        let hash = crypto.createHash('sha256')
-        supertest(schsrch)
-          .get('/doc/' + encodeURIComponent(tDoc._id) + '/?as=blob')
-          .set('Host', 'schsrch.xyz')
-          .expect(200)
-          .expect('Content-Type', /pdf/)
-          .expect(res => res.header['content-length'].should.be.above(0))
-          .buffer()
-          .parse((res, callback) => {
-            res.setEncoding('binary')
-            res.on('data', chunk => {
-              hash.write(chunk)
+    function testBody (withFormat) {
+      return function (done) {
+        PastPaperDoc.find({subject: '0610', time: 's17', paper: 1, variant: 1, type: 'qp'}).then(docs => {
+          if (!docs || docs.length !== 1) {
+            done(new Error(`There should be one and only one 0610_s17_1_1_qp in the testing database (there are currently ${docs.length}).`))
+            return
+          }
+          let tDoc = docs[0]
+          let hash = crypto.createHash('sha256')
+          supertest(schsrch)
+            .get('/doc/' + encodeURIComponent(tDoc._id) + '/' + (withFormat ? '?as=blob' : ''))
+            .set('Host', 'schsrch.xyz')
+            .expect(200)
+            .expect('Content-Type', /pdf/)
+            .expect(res => res.header['content-length'].should.be.above(0))
+            .buffer()
+            .parse((res, callback) => {
+              res.setEncoding('binary')
+              res.on('data', chunk => {
+                hash.write(chunk)
+              })
+              res.on('end', () => {
+                hash.end()
+                callback(null, null)
+              })
             })
-            res.on('end', () => {
-              hash.end()
-              callback(null, null)
-            })
-          })
-          .end(err => {
-            if (err) {
-              done(err)
-              return
-            }
-            hash.on('readable', () => {
-              try {
-                hash.read().toString('hex').should.equal('00a2562f321e764b70a69fa4d374f8ac5aee20731e4a788f2ce4a898f41f262b') // sha256sum test/pastpapers/0610_s17_qp_11.pdf
-                done()
-              } catch (e) {
-                done(e)
+            .end(err => {
+              if (err) {
+                done(err)
+                return
               }
+              hash.on('readable', () => {
+                try {
+                  hash.read().toString('hex').should.equal('00a2562f321e764b70a69fa4d374f8ac5aee20731e4a788f2ce4a898f41f262b') // sha256sum test/pastpapers/0610_s17_qp_11.pdf
+                  done()
+                } catch (e) {
+                  done(e)
+                }
+              })
             })
           })
-        })
-    })
+      }
+    }
+    it('/doc/?as=blob', testBody(true))
+    it('/doc/', testBody(false))
     it('/doc/ with 000000000000000000000000', function (done) {
       supertest(schsrch)
         .get('/doc/000000000000000000000000/?as=blob')
@@ -68,7 +72,7 @@ module.exports = (schsrch, dbModel) =>
           return
         }
         let tDoc = docs.filter(x => x.type === 'qp')[0]
-        let tDocMs = docs.filter(x => x.type === 'ms')[0]
+        let tMsDoc = docs.filter(x => x.type === 'ms')[0]
         sspdfTestDoc = tDoc
         supertest(schsrch)
           .get('/doc/' + encodeURIComponent(tDoc._id) + '/?page=0&as=sspdf')
@@ -86,9 +90,33 @@ module.exports = (schsrch, dbModel) =>
           .expect(res => res.body.svg.should.be.a.String().and.match(/^<svg/))
           .expect(res => res.body.related.should.be.an.Object())
           .expect(res => res.body.related.type.should.equal('ms'))
-          .expect(res => res.body.related._id.should.equal(tDocMs._id.toString()))
-          .end(done)
-        })
+          .expect(res => res.body.related._id.should.equal(tMsDoc._id.toString()))
+          .end(testForMs)
+        function testForMs (err) {
+          if (err) {
+            done(err)
+            return
+          }
+          supertest(schsrch)
+            .get('/doc/' + encodeURIComponent(tMsDoc._id) + '/?page=0&as=sspdf')
+            .set('Host', 'schsrch.xyz')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(res => res.body.should.be.an.Object())
+            .expect(res => res.body.doc.should.be.an.Object())
+            .expect(res => ['subject', 'time', 'paper', 'variant', 'type', 'numPages', 'fileType'].forEach(p => res.body.doc[p].should.equal(tMsDoc[p])))
+            .expect(res => res.body.doc._id.should.equal(tMsDoc._id.toString()))
+            .expect(res => ['width', 'height'].forEach(p => res.body[p].should.be.a.Number().and.above(0)))
+            .expect(res => res.body.pageNum.should.equal(tMsDoc.numPages))
+            .expect(res => should.not.exist(res.body.doc.doc))
+            .expect(res => should.not.exist(res.body.doc.fileBlob))
+            .expect(res => res.body.svg.should.be.a.String().and.match(/^<svg/))
+            .expect(res => res.body.related.should.be.an.Object())
+            .expect(res => res.body.related.type.should.equal('qp'))
+            .expect(res => res.body.related._id.should.equal(tDoc._id.toString()))
+            .end(done)
+        }
+      })
     }
     it('sspdf', sspdfTestBody)
     it('sspdf (second time on the same document)', sspdfTestBody)
