615d34e6eb6af94f6747497d6ba5681ed9304f23
Mao Wtm <micromaomao@gmail.com>
2017-07-24 14:47:03 +0800

    Some browser don't implement Response::clone, so avoid using it.

--- view/fetcherrorpromise.js
+++ view/fetcherrorpromise.js
@@ -2,17 +2,16 @@ const AppState = require('./appstate.js')
 
 module.exports = {
   then: res => new Promise((resolve, reject) => {
-    let _res = res.clone()
-    res.text().then(rspText => {
-      if (!res.ok) {
+    if (!res.ok) {
+      res.text().then(rspText => {
         reject(new Error(`${(rspText.length < 100 ? ` ${rspText}` : '')} (HTTP status: ${res.status})`))
         if (rspText.trim() === 'Authorization token invalid.') {
           AppState.dispatch({type: 'clear-token'})
         }
-      } else {
-        resolve(_res)
-      }
-    }, err => reject(new Error(res.status)))
+      }, reject)
+    } else {
+      resolve(res)
+    }
   }),
   error: err => new Promise((resolve, reject) => {
     reject(new Error('Network unstable or SchSrch has crashed.'))
