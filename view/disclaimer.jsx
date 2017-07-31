9e9aff6d45b39b612fb7257a650a987a84e922d3
Mao Wtm <micromaomao@gmail.com>
2017-07-31 12:12:03 +0800

    Rewrite disclaimer

--- view/disclaimer.jsx
+++ view/disclaimer.jsx
@@ -24,10 +24,17 @@ class Disclaimer extends React.Component {
         <h1>Disclaimer</h1>
         <div className='tldr'>tl; dr</div>
         <ul>
-          <li>This program itself, which is a part of maowtm.org, is free software. It comes without any warranty, to the extent permitted by applicable law. You can redistribute it and/or modify it under the terms of the MIT License.</li>
-          <li>The past papers hosted here is copyrighted by UCLES. PDF files is gathered from the Web.</li>
-          <li>For educational purpose, I can't account for copyright issues on Cambridge papers. Many websites are doing this anyway.</li>
-          <li>This website is provided AS-IS, with no guarantee of correctness. This website is entirely non-profit. No advertisements shall appear.</li>
+          <li>This program itself is free software. It comes without any warranty, to the extent permitted by applicable law. You can redistribute it and/or modify it under the terms of the MIT License.</li>
+          <li>The past papers hosted here is copyrighted by&nbsp;
+            {this.state.server ?
+              (
+                <a href='http://www.cambridgeassessment.org.uk/' target='_blank'>UCLES</a>
+              ) :
+              (
+                <a onClick={evt => window.open('http://www.cambridgeassessment.org.uk/')}>UCLES</a>
+              )}. PDF files is gathered from the Web.</li>
+          <li>All papers are provided solely for educational purpose.</li>
+          <li>This website and its content is provided AS-IS, with no guarantee of correctness, and is entirely non-profit.</li>
         </ul>
       </div>
     )
