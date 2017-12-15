80e8251d94bc119451edbea66f065c6076a1b046
Mao Wtm <micromaomao@gmail.com>
2017-12-15 21:13:09 +0800

    More debugging-friendly perpareDatabase.sh

--- test/perpareDatabase.sh
+++ test/perpareDatabase.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+paperbin=pastpapers.bin
+password=schsrch
 
 # Drop database and elasticsearch index.
 mongo --host $MONGODB --eval "db.getSiblingDB('test').dropDatabase()"
@@ -7,13 +9,19 @@ curl -X DELETE 'http://'$ES'/pastpaper'
 
 # Extract papers
 tmpdir=$(mktemp -d)
-openssl aes-256-cbc -d -in pastpapers.bin -pass pass:schsrch | gzip -d | tar -x -C $tmpdir
+openssl aes-256-cbc -d -in $paperbin -pass pass:$password -out $tmpdir/decrypt
+if [ $? -ne 0 ]; then
+  echo "Unable to decrypt $paperbin"
+  rm -rv $tmpdir
+  exit 1
+fi
+gzip -d < $tmpdir/decrypt | tar -x -C $tmpdir
 if [ $? -ne 0 ]; then
   echo "Unable to extract past papers to $tmpdir."
   rm -rv $tmpdir
   exit 1
 fi
-if [ ! -f $tmpdir/pastpapers/9699_s17_1_3_qp.pdf]; then
+if [ ! -f $tmpdir/pastpapers/9699_s17_1_3_qp.pdf ]; then
   echo "Expected extracted paper to exist in $tmpdir."
   rm -rv $tmpdir
   exit 1
