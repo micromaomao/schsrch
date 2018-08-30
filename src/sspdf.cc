95fafbe9fb70bb9446051bb9d339186028802bf9
Mao Wtm <micromaomao@gmail.com>
2018-08-31 00:28:01 +0800

    SSPDF: Rewrite to make extracting only text and layouts more efficient.
    Also sncludes a rewrited ./doIndex.bin.js

--- src/sspdf.cc
+++ src/sspdf.cc
@@ -34,6 +34,7 @@ const char* MSG_EXCEPTION_PAGE_OUT_OF_RANGE = "Page index out of range.";
 const char* MSG_EXCEPTION_CARIO_ERROR = "Error creating cario surface.";
 
 class PdfssWorker : public AsyncWorker {
+  // DEPRECATION PLANNED
   public:
     char* pdfData = NULL;
     size_t pdfLen = 0;
@@ -173,6 +174,139 @@ class PdfssWorker : public AsyncWorker {
     }
 };
 
+class pdfContentAllWorker : public AsyncWorker {
+  public:
+    char* pdfData = NULL;
+    size_t pdfLen = 0;
+    double pw, ph;
+    char** pageTexts = NULL;
+    PopplerRectangle** pageRects = NULL;
+    guint* pageRectLens = NULL;
+    char* error = NULL; // Error stored here from {constructor, Execute} freed in HandleOKCallback.
+    int numPages = 0;
+    // pdf data copied.
+    pdfContentAllWorker(Callback* cb, Local<Uint8Array> hPdf)
+      : AsyncWorker(cb) {
+      this->pdfLen = (*hPdf)->ByteLength();
+      if (this->pdfLen == 0) {
+        this->error = new char[strlen(MSG_EXCEPTION_ZEROLEN) + 1];
+        strcpy(this->error, MSG_EXCEPTION_ZEROLEN);
+      } else {
+        char* pdfData = ((char*) (*(*hPdf)->Buffer())->GetContents().Data()) + (*hPdf)->ByteOffset();
+        this->pdfData = new char[this->pdfLen];
+        memcpy(this->pdfData, pdfData, this->pdfLen);
+      }
+    }
+
+    void Execute () {
+      if (this->error != NULL) {
+        return;
+      }
+      GError* gerror = NULL;
+      PopplerDocument* popperDoc = poppler_document_new_from_data(this->pdfData, this->pdfLen, NULL, &gerror);
+      if (popperDoc == NULL) {
+        this->error = new char[strlen(gerror->message) + 1];
+        strcpy(this->error, gerror->message);
+        g_error_free(gerror);
+        gerror = NULL;
+        return;
+      }
+      int nPages = poppler_document_get_n_pages(popperDoc);
+      this->numPages = nPages;
+
+      this->pageRects = new PopplerRectangle*[nPages];
+      this->pageTexts = new char*[nPages];
+      this->pageRectLens = new guint[nPages];
+      for (int p = 0; p < nPages; p ++) {
+        PopplerPage* page = poppler_document_get_page(popperDoc, p);
+        if (page == NULL) {
+          this->error = new char[strlen(MSG_EXCEPTION_PAGE_FAIL) + 1];
+          strcpy(this->error, MSG_EXCEPTION_PAGE_FAIL);
+          g_object_unref(popperDoc);
+          return;
+        }
+
+        poppler_page_get_size(page, &this->pw, &this->ph);
+        // this->pageTexts[p] = poppler_page_get_text(page);
+        auto pageText = poppler_page_get_text(page);
+        this->pageTexts[p] = new char[strlen(pageText) + 1];
+        strcpy(this->pageTexts[p], pageText);
+        g_free(pageText);
+        pageText = NULL;
+        poppler_page_get_text_layout(page, this->pageRects + p, this->pageRectLens + p);
+
+        g_object_unref(page);
+      }
+      g_object_unref(popperDoc);
+      popperDoc = NULL;
+    }
+
+    void HandleOKCallback () {
+      HandleScope scope;
+      Local<Object> returnObj = New<Object>();
+      Local<Array> pageRects = New<Array>();
+      Local<Array> pageTexts = New<Array>();
+      Local<Value> argv[] = {
+        Null(), Null()
+      };
+      if (this->error == NULL) {
+        auto strLastWidth = New<v8::String>("lastWidth").ToLocalChecked();
+        auto strLastHeight = New<v8::String>("lastHeight").ToLocalChecked();
+        auto strNumPages = New<v8::String>("numPages").ToLocalChecked();
+        auto strPageTexts = New<v8::String>("pageTexts").ToLocalChecked();
+        auto strPageRects = New<v8::String>("pageRects").ToLocalChecked();
+        auto strX1 = New<v8::String>("x1").ToLocalChecked();
+        auto strX2 = New<v8::String>("x2").ToLocalChecked();
+        auto strY1 = New<v8::String>("y1").ToLocalChecked();
+        auto strY2 = New<v8::String>("y2").ToLocalChecked();
+        Set(returnObj, strLastWidth, New<v8::Number>(this->pw));
+        Set(returnObj, strLastHeight, New<v8::Number>(this->ph));
+        Set(returnObj, strNumPages, New<v8::Number>(this->numPages));
+        for (int p = 0; p < this->numPages; p ++) {
+          auto pn = New<v8::Number>(p);
+          Set(pageTexts, pn, New<v8::String>(this->pageTexts[p]).ToLocalChecked());
+          delete this->pageTexts[p];
+          this->pageTexts[p] = NULL;
+          auto rects = New<Array>();
+          auto cRects = this->pageRects[p];
+          auto cRectlen = this->pageRectLens[p];
+          for (guint i = 0; i < cRectlen; i ++) {
+            Local<Object> rectObj = New<Object>();
+            auto cRect = cRects[i];
+            Set(rectObj, strX1, New<v8::Number>(cRect.x1));
+            Set(rectObj, strX2, New<v8::Number>(cRect.x2));
+            Set(rectObj, strY1, New<v8::Number>(cRect.y1));
+            Set(rectObj, strY2, New<v8::Number>(cRect.y2));
+            Set(rects, New<v8::Number>(i), rectObj);
+          }
+          Set(pageRects, New<v8::Number>(p), rects);
+          if (cRectlen) g_free(cRects);
+          this->pageRects[p] = NULL;
+        }
+        Set(returnObj, strPageTexts, pageTexts);
+        Set(returnObj, strPageRects, pageRects);
+        argv[1] = returnObj;
+      } else {
+        argv[0] = v8::Exception::Error(New<v8::String>(this->error).ToLocalChecked());
+        delete this->error;
+        this->error = NULL;
+      }
+      this->callback->Call(2, argv);
+      if (this->pageRects) {
+        delete this->pageRects;
+      }
+      if (this->pageTexts) {
+        delete this->pageTexts;
+      }
+      if (this->pageRectLens) {
+        delete this->pageRectLens;
+      }
+      if (this->pdfData != NULL) {
+        delete this->pdfData;
+      }
+    }
+};
+
 NAN_METHOD(getPage) {
   if (info.Length() != 3) {
     ThrowTypeError("getPage(pdfBuffer, pageNum, callback)");
@@ -204,10 +338,35 @@ NAN_METHOD(getPage) {
   AsyncQueueWorker(new PdfssWorker(callback, pdfBuffer, pn));
 }
 
+NAN_METHOD(getPDFContentAll) {
+  if (info.Length() != 2) {
+    ThrowTypeError("getPage(pdfBuffer, callback)");
+    return;
+  }
+  if (!info[0]->IsUint8Array()) {
+    ThrowTypeError("arg[0] is not a buffer.");
+    return;
+  }
+  if (!info[1]->IsFunction()) {
+    ThrowTypeError("arg[1] is not a function.");
+    return;
+  }
+  auto pdfBuffer = info[0].As<Uint8Array>();
+  if (pdfBuffer.IsEmpty()) {
+    ThrowTypeError("arg[0] can't resolve.");
+    return;
+  }
+  Callback *callback = new Callback(info[1].As<Function>());
+  AsyncQueueWorker(new pdfContentAllWorker(callback, pdfBuffer));
+}
+
 NAN_MODULE_INIT(Init) {
   Set(target
     , New<v8::String>("getPage").ToLocalChecked()
     , New<v8::FunctionTemplate>(getPage)->GetFunction());
+  Set(target
+    , New<v8::String>("getPDFContentAll").ToLocalChecked()
+    , New<v8::FunctionTemplate>(getPDFContentAll)->GetFunction());
 }
 
 NODE_MODULE(sspdf, Init)
