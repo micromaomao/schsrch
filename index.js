cfa8c42ca6f70c272195cff494d5f349c058a496
Mao <m@maowtm.org>
2023-10-28 01:13:38 +0100

    Remove sw.js endpoint

--- index.js
+++ index.js
@@ -111,10 +111,6 @@ module.exports = ({mongodb: db, elasticsearch: es, siteOrigin}) => {
     })
     rMain.use('/resources', express.static(path.join(__dirname, 'dist')))
     rMain.use('/resources/pdfjs', express.static(path.join(__dirname, 'node_modules/pdfjs-dist/build')))
-    rMain.get('/sw.js', function (req, res) {
-      res.set('cache-control', 'max-age=0')
-      res.sendFile(path.join(__dirname, 'dist/sw.js'))
-    })
     rMain.get('/opensearch.xml', function (req, res) {
       res.set('cache-control', 'max-age=0')
       res.sendFile(path.join(__dirname, 'view/opensearch.xml'), {
